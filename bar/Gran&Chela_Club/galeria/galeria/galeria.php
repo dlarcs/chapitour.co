4a2fed34a29b6546b5a19cf1470587924c8c0153
Laura  Rozo <admin@promoflow.net>
2026-07-09 11:20:19 -0500

    “…”

--- bar/Gran&Chela_Club/galeria/galeria/galeria.php
+++ bar/Gran&Chela_Club/galeria/galeria/galeria.php
@@ -21,29 +21,20 @@ $jsVer  = is_file($jsFile) ? filemtime($jsFile) : '';
   </p>
 
   <div class="gallery-grid">
-    <img src="../../../bar/Gran&Chela_Club/img/general.png" alt="Ambiente de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general1.png" alt="Barra de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general2.png" alt="Zona social de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general3.png" alt="Decoración de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general4.png" alt="Luces de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general5.png" alt="Espacio para compartir en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general6.png" alt="Interior moderno de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general7.png" alt="Ambiente nocturno de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general8.png" alt="Zona de bebidas de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general9.png" alt="Mesa de Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general10.png" alt="Experiencia en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general11.png" alt="Noche especial en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general12.png" alt="Música en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general13.png" alt="Celebración en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general14.png" alt="Cocteles en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general15.png" alt="Encuentro entre amigas en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general16.png" alt="Ambiente queer en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general17.jpg" alt="Espacio seguro en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general18.jpg" alt="Fiesta en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general19.png" alt="Evento especial en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general20.png" alt="Noche de baile en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general21.png" alt="Compartir en Capital Queer">
-    <img src="../../../bar/Gran&Chela_Club/img/general22.png" alt="Experiencia nocturna en Capital Queer">
+    <img src="../../../bar/Gran&Chela_Club/img/general.jpg" alt="Ambiente de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general1.jpg" alt="Barra de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general2.jpg" alt="Zona social de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general3.jpg" alt="Decoración de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general4.jpg" alt="Luces de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general5.jpg" alt="Espacio para compartir en Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general6.jpg" alt="Interior moderno de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general7.jpg" alt="Ambiente nocturno de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general8.jpg" alt="Zona de bebidas de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general9.jpg" alt="Mesa de Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general10.jpg" alt="Experiencia en Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general11.jpg" alt="Noche especial en Gran&Chelas Club">
+    <img src="../../../bar/Gran&Chela_Club/img/general12.jpg" alt="Música en Gran&Chelas Club">
+
   </div>
 
 </section>
