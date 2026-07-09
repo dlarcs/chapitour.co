98035a666d730b339bccbec1f9e3d658b6e33382
Laura  Rozo <admin@promoflow.net>
2026-07-09 09:58:57 -0500

    “…”

--- bar/CapitalQueer/index.php
+++ bar/CapitalQueer/index.php
@@ -69,7 +69,7 @@
         "@id": "https://chapitour.co/bar/CapitalQueer/#business",
         "name": "Capital Queer",
         "description": "Bar en Chapinero, Bogotá, creado especialmente para mujeres y abierto a todas las personas que quieran compartir con respeto, libertad y buena energía.",
-        "url": "https://https://chapitour.co/bar/CapitalQueer/index.php",
+        "url": "https://chapitour.co/bar/CapitalQueer/index.php",
         "image": "https://chapitour.co/bar/CapitalQueer/img/general17.jpg",
         "telephone": "+573007795016",
         "servesCuisine": "Bar",
