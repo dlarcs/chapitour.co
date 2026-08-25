c4d1bb47c4062ca0647b7eb01a7f625f1ac3de7e
Laura  Rozo <admin@promoflow.net>
2026-08-25 09:41:41 -0500

    “…”

--- juegos/JimarFactory/home/galeria/galeria.php
+++ juegos/JimarFactory/home/galeria/galeria.php
@@ -1,238 +1,52 @@
 <?php
 $base = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
 
-$cssFile = $base . '/juegos/JimarFactory/galeria/galeria/galeria.css';
-$jsFile  = $base . '/juegos/JimarFactory/galeria/galeria/galeria.js';
+$cssFile = $base . '/juegos/JimarFactory/home/galeria/galeria.css';
+$jsFile  = $base . '/juegos/JimarFactory/home/galeria/galeria.js';
 
 $cssVer = is_file($cssFile) ? filemtime($cssFile) : '';
 $jsVer  = is_file($jsFile) ? filemtime($jsFile) : '';
 ?>
 
-<link rel="stylesheet" href="../../../juegos/JimarFactory/galeria/galeria/galeria.css<?= $cssVer ? '?v=' . $cssVer : '' ?>">
+<link rel="stylesheet" href="../../juegos/JimarFactory/home/galeria/galeria.css<?= $cssVer ? '?v=' . $cssVer : '' ?>">
 
 <section class="business-section business-gallery visible">
   <span class="section-label">Galería</span>
 
-  <h2>Conoce el ambiente de Jimar Factory Chapinero</h2>
+  <h2>Conoce Jimar Factory Chapinero</h2>
 
   <p>
     Disfruta del mejor ambiente de billar en Bogotá, con mesas de tres bandas y
-    pool, bebidas, cócteles, café y entretenimiento para mayores de 18 años.
-    Visítanos en la calle 58 #13-93, de domingo a domingo, de 9:00 a. m. a
-    12:00 a. m.
+    pool, variedad de bebidas, cócteles, café y entretenimiento para mayores de
+    18 años. Estamos ubicados en la calle 58 #13-93 y atendemos todos los días,
+    de 9:00 a. m. a 12:00 a. m.
   </p>
 
   <div class="gallery-grid">
     <img
-      src="../../../juegos/JimarFactory/img/general.jpeg"
-      alt="Ambiente de Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general1.jpeg"
+      src="../../juegos/JimarFactory/img/general3.jpeg"
       alt="Mesas de billar en Jimar Factory Chapinero"
     >
 
     <img
-      src="../../../juegos/JimarFactory/img/general2.jpeg"
-      alt="Partida de billar a tres bandas en Jimar Factory Chapinero"
+      src="../../juegos/JimarFactory/img/general17.jpeg"
+      alt="Ambiente de Jimar Factory Chapinero"
     >
 
     <img
-      src="../../../juegos/JimarFactory/img/general3.jpeg"
-      alt="Mesas de pool en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general4.jpeg"
-      alt="Jugadores disfrutando del billar en Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general5.jpeg"
-      alt="Zona social de Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general6.jpeg"
-      alt="Interior de Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general7.jpeg"
-      alt="Ambiente nocturno de Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general8.jpeg"
-      alt="Zona de bebidas de Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general9.jpeg"
-      alt="Mesa profesional de billar en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general10.jpeg"
-      alt="Experiencia de billar y entretenimiento en Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general11.jpeg"
-      alt="Encuentro entre amigos en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general12.jpeg"
-      alt="Servicio de billar pool en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general13.jpeg"
-      alt="Celebraciones en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general14.jpeg"
-      alt="Cócteles y bebidas en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general15.jpeg"
-      alt="Café y servicio de bebidas en Jimar Factory Chapinero"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general16.jpeg"
-      alt="Materiales y accesorios para jugar billar"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general17.jpeg"
-      alt="Jimar Factory Chapinero en la calle 58 número 13-93"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general18.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 18"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general19.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 19"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general20.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 20"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general21.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 21"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general22.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 22"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general23.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 23"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general24.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 24"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general25.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 25"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general26.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 26"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general27.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 27"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general28.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 28"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general29.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 29"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general30.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 30"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general31.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 31"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general32.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 32"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general33.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 33"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general34.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 34"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general35.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 35"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general36.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 36"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general37.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 37"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general38.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 38"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general39.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 39"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general40.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 40"
-    >
-
-    <img
-      src="../../../juegos/JimarFactory/img/general41.jpeg"
-      alt="Galería de Jimar Factory Chapinero imagen 41"
+      src="../../juegos/JimarFactory/img/general15.jpeg"
+      alt="Zona social y de bebidas de Jimar Factory Chapinero"
+      class="gallery-img-mobile-hide"
     >
   </div>
+
+  <a href="../../juegos/JimarFactory/galeria/index.php">
+    <div class="button_container">
+      <button class="btn btn30" type="button" name="button">
+        Ver galería
+      </button>
+    </div>
+  </a>
 </section>
 
-<script defer src="../../../juegos/JimarFactory/galeria/galeria/galeria.js<?= $jsVer ? '?v=' . $jsVer : '' ?>"></script>
+<script defer src="../../juegos/JimarFactory/home/galeria/galeria.js<?= $jsVer ? '?v=' . $jsVer : '' ?>"></script>
