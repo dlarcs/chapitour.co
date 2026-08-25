f9adf3ed612b36b9594682ad2f59b11a0b0cd025
Laura  Rozo <admin@promoflow.net>
2026-08-25 11:04:19 -0500

    “…”

--- juegos/JimarFactory/global/pag_footer/pag_footer.php
+++ juegos/JimarFactory/global/pag_footer/pag_footer.php
@@ -19,17 +19,17 @@ $cssVer = is_file($cssFile) ? filemtime($cssFile) : '';
 
     <div class="footer-col">
       <h5>Categorías</h5>
-      <a href="categorias/1.tipo_bares/index.php">Bar</a>
-      <a href="categorias/7.gastronomia/index.php">Gastro Bar</a>
-      <a href="categorias/3.cafes/index.php">Café</a>
-      <a href="categorias/2.planes_bares/index.php">Juegos</a>
-      <a href="categorias/5.LGBTIQ+/index.php">LGBTIQ+</a>
+      <a href="../../../categorias/1.tipo_bares/index.php">Bar</a>
+      <a href="../../../categorias/7.gastronomia/index.php">Gastro Bar</a>
+      <a href="../../../categorias/3.cafes/index.php">Café</a>
+      <a href="../../../categorias/2.planes_bares/index.php">Juegos</a>
+      <a href="../../../categorias/5.LGBTIQ+/index.php">LGBTIQ+</a>
     </div>
 
     <div class="footer-col">
       <h5>De interes</h5>
-      <a href="categorias/category/index.php">Categorías</a>
-      <a href="categorias/10.destacados/index.php">Destacados</a>
+      <a href="../../../categorias/category/index.php">Categorías</a>
+      <a href="../../../categorias/10.destacados/index.php">Destacados</a>
       <a href="https://Arbelaez.com.co">Arbelaez.com.co</a>
     </div>
 
